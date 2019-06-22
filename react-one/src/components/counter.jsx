aafecf4671f73c36e2718a879f787dda05f0d3d7
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-22 20:39:46 +0200

    Embedding Expressions

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Incement</button>
       </React.Fragment>
     );
