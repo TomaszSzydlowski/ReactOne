1b87b560a3106d9b4ea6a93013c372dfa03922b1
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-23 10:38:33 +0200

    Conditional Rendering

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -3,32 +3,28 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags</p>;
+
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Incement</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please set new elements"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
