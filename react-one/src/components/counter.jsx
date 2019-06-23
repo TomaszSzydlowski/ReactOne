f2b513c4615dff8525ab15d4949e2739eac6ab25
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-23 13:36:09 +0200

    Updating the State

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -11,7 +11,7 @@ class Counter extends Component {
   // }
 
   handleIncrement = () => {
-    console.log("Increment", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
