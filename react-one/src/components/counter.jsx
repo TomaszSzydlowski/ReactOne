d607ff945ca712cd69a30747d890a27c3f64637c
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-24 21:48:59 +0200

    Added example

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   // constructor() {
@@ -15,19 +16,38 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>
+            <React.Fragment>
+              <span className={this.getBadgeClasses()}>
+                {this.formatCount()}
+              </span>
+              <button
+                onClick={() => {
+                  this.handleIncrement({ id: 1 });
+                }}
+                className="btn btn-secondary btn-sm"
+              >
+                Incement
+              </button>
+            </React.Fragment>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => {
-            this.handleIncrement({ id: 1 });
-          }}
-          className="btn btn-secondary btn-sm"
-        >
-          Incement
-        </button>
-      </React.Fragment>
+      <div>
+        {this.state.tags.length === 0 && "Please set new elements"}
+        {this.renderTags()}
+      </div>
     );
   }
 
