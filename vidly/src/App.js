654aa7ea31a9aeb89427e54aaf5c18dd41e061a7
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-25 15:56:55 +0200

    Example exc

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,10 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import Movies from "./components/movies";
 
-function App() {
-  return (
-    <main className="container">
-      <h1>Hello world</h1>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return <Movies />;
+  }
 }
 
 export default App;
