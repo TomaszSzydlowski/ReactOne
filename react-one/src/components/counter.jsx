9ac6c404b5ffc03f6416a23f4aa7760b8159fc71
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-26 14:53:58 +0200

    Cleaning

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -16,28 +16,33 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There is no tags</p>;
 
     return (
       <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>
-            <React.Fragment>
-              <span className={this.getBadgeClasses()}>
-                {this.formatCount()}
-              </span>
-              <button
-                onClick={() => {
-                  this.handleIncrement({ id: 1 });
-                }}
-                className="btn btn-secondary btn-sm"
-              >
-                Incement
-              </button>
-            </React.Fragment>
-          </li>
-        ))}
+        <React.Fragment>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button
+            onClick={() => {
+              this.handleIncrement({ id: 1 });
+            }}
+            className="btn btn-secondary btn-sm"
+          >
+            Incement
+          </button>
+        </React.Fragment>
       </ul>
     );
   }
