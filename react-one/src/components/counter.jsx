183987ea3f427d6e4cf340725b21b3f461be6d3d
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-27 09:45:18 +0200

    Passing Children

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -29,6 +29,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => {
