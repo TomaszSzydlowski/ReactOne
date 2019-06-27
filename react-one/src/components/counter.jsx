c352efc23d29cb19aa299fceadc4064049c69773
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-27 14:45:03 +0200

    Raising and Handling Events

--- react-one/src/components/counter.jsx
+++ react-one/src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.counter.value
   };
 
   // constructor() {
@@ -29,7 +28,6 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => {
@@ -38,6 +36,14 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Incement
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete(this.props.counter.id);
+          }}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
