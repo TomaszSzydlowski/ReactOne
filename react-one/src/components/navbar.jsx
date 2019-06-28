bcf372c1ceb56a7e8d2e2f2ca0c07a2b5e68ef06
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-06-28 21:07:15 +0200

    refactor

--- react-one/src/components/navbar.jsx
+++ react-one/src/components/navbar.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
 const NavBar = ({ totalCounters }) => {
   return (
-    <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-seconday">{totalCounters}</span>
       </a>
