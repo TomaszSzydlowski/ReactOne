98dc0adc2c2c1d1ef100beb04c0e2d3906e38cba
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-07-04 21:11:47 +0200

    added track e.currentTarget.value

--- react-one/src/components/loginForm.jsx
+++ react-one/src/components/loginForm.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
-
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  state = {
+    account: { username: "", password: "" }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
 
-    const username = this.username.current.value;
     console.log("Submitted");
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
   };
 
   render() {
@@ -23,7 +26,8 @@ class LoginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
-              ref={this.username}
+              value={this.state.account.username}
+              onChange={this.handleChange}
               id="username"
               type="text"
               className="form-control"
