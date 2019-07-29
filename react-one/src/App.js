5b1245eb0295f561f7aeae8c9cbd7d2fd4bdf457
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2019-07-29 16:00:25 +0200

    Logging out a User

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -10,6 +10,7 @@ import Customers from "./components/customers";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerform";
+import Logout from "./components/logout";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
@@ -32,6 +33,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
